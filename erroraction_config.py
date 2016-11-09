f26e3d79f465080549b7a983308998eff78bde53
Orsiris de Jong <ozy@netpower.fr>
2016-11-09 23:45:43 +0100

    Initial upload of python alert configuration tool

---
+++ erroraction_config.py
@@ -0,0 +1,417 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#### BASIC FUNCTIONS & DEFINITIONS #########################################################################
+
+class Constants:
+	"""Simple class to use constants
+	usage
+	_CONST = Constants
+	print(_CONST.NAME)
+	"""
+	APP_NAME="erroraction_config"
+	APP_VERSION="0.1"
+	APP_BUILD="2016110901"
+	APP_DESCRIPTION="smartmontools for Windows mail config"
+	CONTACT="ozy@netpower.fr - http://www.netpower.fr"
+	AUTHOR="Orsiris de Jong"
+	
+	ERRORACTION_CMD_FILENAME="erroraction_config.cmd"
+	MAILSEND_BINARY="mailsend.exe"
+	
+	IS_STABLE=False
+
+	LOG_FILE=APP_NAME + ".log"
+
+	def __setattr__(self, *_):
+		pass
+    
+_CONSTANT = Constants
+
+#### LOGGING & DEBUG CODE ####################################################################################
+
+import os
+
+try:
+	os.environ["_DEBUG"]
+	_DEBUG = True
+except:
+	_DEBUG = False
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Set file log
+logFileHandler = RotatingFileHandler(_CONSTANT.LOG_FILE, mode='a', encoding='utf-8', maxBytes=1000000, backupCount=1)
+logFileHandler.setLevel(logging.DEBUG)
+logFileHandler.setFormatter(logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s'))
+logger.addHandler(logFileHandler)
+
+# Set stdout log
+logStdoutHandler = logging.StreamHandler()
+if _DEBUG == True:
+	logStdoutHandler.setLevel(logging.DEBUG)
+else:
+	logStdoutHandler.setLevel(logging.ERROR)
+logger.addHandler(logStdoutHandler)
+
+#### IMPORTS ################################################################################################
+
+import sys, getopt
+
+import platform									# Detect OS
+import re										# Regex handling
+import time										# sleep command
+import codecs									# unicode encoding
+from subprocess import Popen, PIPE				# call to external binaries
+
+from datetime import datetime
+
+# GUI
+try:
+	import tkinter as tk		# Python 3
+	from tkinter import messagebox
+except:
+	import Tkinter as tk		# Python 2
+	import tkMessageBox as messagebox
+import pygubu					# GUI builder
+
+logger.info("Running on python " + platform.python_version() + " / " + str(platform.uname()))
+
+#### ACTUAL APPLICATION ######################################################################################
+
+CONFIG = "" # Contains full config as Configuration class
+
+class Configuration:
+	errorActionCmdPath = ""
+	
+	def __init__(self, filePath= ''):
+		"""Determine smartd configuration file path"""
+		
+		# __file__ variable doesn't exist in frozen py2exe mode, get appRoot
+		try:
+			self.appRoot = os.path.dirname(os.path.abspath(__file__))
+		except:
+			self.appRoot = os.path.dirname(os.path.abspath(sys.argv[0]))
+		
+		try:
+			#TODO: undo double checked, here and in readconf
+			if not os.path.isfile(filePath):
+				raise TypeError
+			else:
+				self.errorActionCmdPath = filePath
+		except:
+			#rootWindow.widthdraw()
+			msg="Wrong configuration file path provided."
+			logger.critical(msg)
+			messagebox.showinfo("Error", msg)
+			sys.exit(1)
+
+class Application:
+	"""pygubu tkinter GUI class"""
+	
+	configValues = ['MAIL_ALERT', 'SOURCE_MAIL', 'DESTINATION_MAIL', 'SMTP_SERVER', 'SMTP_PORT',
+	'LOCAL_ALERT', 'WARNING_MESSAGE', 'COMPRESS_LOGS', 'DEVICE_LIST', 'SMART_LOG_FILE', 'ERROR_LOG_FILE', 'PROGRAM_PATH']
+					
+	authValues = ['SMTP_USER', 'SMTP_PASSWORD', 'SECURITY']
+	
+	def __init__(self, master, configDict):
+		self.master = master
+		self.configDict = configDict
+		self.builder = builder = pygubu.Builder()
+
+		# Load GUI xml description file
+		filePath = os.path.join(CONFIG.appRoot, _CONSTANT.APP_NAME + ".ui")
+		try:
+			self.builder.add_from_file(filePath)
+		except Exception as e:
+			logger.critical("Cannot find ui file [" + filePath + "].")
+			logger.debug(e)
+			sys.exit(1)
+
+		self.mainwindow = builder.get_object('MainFrame', master)
+		
+		# Bind GUI actions to functions
+		self.builder.connect_callbacks(self)
+		callbacks = {
+			'onUseAuthentication': self.onUseAuthentication,
+			'onSendTestEmail': self.onSendTestEmail,
+			'onSaveAndExit': self.onSaveAndExit,
+		}
+		self.builder.connect_callbacks(callbacks)
+
+		# Read config values but prefer passed arguments over read ones
+		tempDict = self.configDict
+		self.configDict = readErrorConfigFile(CONFIG.errorActionCmdPath)
+		self.configDict.update(tempDict)
+		
+		# Populate values in GUI
+		for key, value in self.configDict.items():
+			try:
+				try:
+					self.builder.get_object(key, master).delete("1.0", "end")
+				except:
+					self.builder.get_object(key, master).delete("0", "end")
+
+				self.builder.get_object(key, master).insert("end", value)
+				self.builder.get_variable(key).set(value)
+			except:
+				if value == "yes":
+					try:
+						self.builder.get_object(key, master).select()
+					except:
+						pass
+				elif value == "no":
+					try:
+						self.builder.get_object(key, master).deselect()
+					except:
+						pass
+
+		if "SMTP_USER" in self.configDict:
+			self.builder.get_object('UseAuthentication', master).select()
+			self.onUseAuthentication()
+
+	def prepareConfigDict(self):
+		values = self.configValues
+		if self.builder.get_variable('useAuthentication').get() == True:
+			values.extend(self.authValues)
+			
+		for key in values:
+			try:
+				self.configDict[key] = self.builder.get_object(key, self.master).get()
+			except:
+				pass
+			try:
+				self.configDict[key] = self.builder.get_object(key, self.master).get("1.0", "end")
+			except:
+				pass
+			try:
+				self.configDict[key] = self.builder.get_variable(key).get()
+			except:
+				pass
+
+		logger.debug(self.configDict)
+	
+	def onUseAuthentication(self):
+		if self.builder.get_variable('useAuthentication').get() == True:
+			background = "#aaffaa"
+		else:
+			background = "#aaaaaa"
+		
+		for key in self.authValues:
+			self.builder.get_object(key)['background']=background
+	
+	def onSendTestEmail(self):
+		sendTestEmail(self.configDict)
+		
+	def onSaveAndExit(self):
+		try:
+			self.prepareConfigDict()
+			writeErrorConfigFile(CONFIG.errorActionCmdPath, self.configDict)
+			messagebox.showinfo('Information', 'Configuration saved')
+		except:
+			messagebox.showinfo('Error', 'Guru meditation failure !')
+			return False
+			
+		sys.exit(0)
+
+def readErrorConfigFile(fileName):
+	if not os.path.isfile(fileName):
+		logger.info("No suitable [" + _CONSTANT.ERRORACTION_CMD_FILENAME + "] file found, creating new file [" + CONFIG.errorActionCmdPath + "].")
+		
+		return False
+	
+	try:
+		fileHandle = open(fileName, 'r')
+	except Exception as e:
+		msg="Cannot open config file [ " + fileName + "]."
+		logger.error(msg)
+		logger.debug(e)
+		messagebox.showinfo("Error", msg)
+		return False
+		
+	try:
+		configDict = {}
+		for line in fileHandle.readlines():
+			if not line[0] == "#" and not line[0] == " " and not line[0] == "\n" and not line[0] == "\r":
+				conf = line.split('=')
+				if len(conf) > 1:
+					if len(conf) > 2:
+						# Handle special case where base 64 encoding uses '=' sign which is also used as separator
+						configDict[conf[0].lstrip('set ')] = conf[1].strip() + '='
+					else:
+						configDict[conf[0].lstrip('set ')] = conf[1].strip()
+	except Exception as e:
+		msg="Cannot read in config file [" + fileName + "]."
+		logger.error(msg)
+		logger.debug(e)
+		messagebox.showinfo("Error", msg)
+		return False
+		
+	try:
+		fileHandle.close()
+		return configDict
+	except Exception as e:
+		logger.error("Cannot close file [" + fileName + "].")
+		logger.debug(e)
+
+def writeErrorConfigFile(fileName, configDict):
+	configLine = ""
+	for key, value in configDict.items():
+		configLine += key + "=" + value + "\n"
+	
+	try:
+		fileHandle = open(fileName, 'w')
+	except Exception as e:
+		msg="Cannot open config file [ " + fileName + "]."
+		logger.error(msg)
+		logger.debug(e)
+		messagebox.showinfo("Error", msg)
+		return False
+		
+	try:
+		fileHandle.write("# This file was generated on " + str(datetime.now()) + " by " + _CONSTANT.APP_NAME + " " + _CONSTANT.APP_VERSION  + "\n# http://www.netpower.fr\n")
+		fileHandle.write(configLine)
+	except Exception as e:
+		msg="Cannot write config file [ " + fileName + "]."
+		logger.error(msg)
+		logger.debug(e)
+		messagebox.showinfo("Error", msg)
+		return False
+	try:
+		fileHandle.close()
+	except Exception as e:
+		logger.error("Cannot close file [" + fileName + "].")
+		logger.debug(e)
+
+def sendTestEmail(configDict):
+	mailsend = CONFIG.appRoot + os.sep + _CONSTANT.MAILSEND_BINARY
+	if not os.path.isfile(mailsend):
+		msg="Cannot find [" + mailsend + "]."
+		logger.error(msg)
+		messagebox.showinfo("Error", msg)
+		return False
+	
+	subject = '\"Smartmontools for Windows setup test mail on %COMPUTERNAME%.%USERDOMAIN% (DNS: %USERDNSDOMAIN%)\"'
+	message = '\"This is a test message sent by Smartmontools for Windows configure tool to check whether the smart service will be able to send you information about your hard drive health. There is no real alert, please do not respond."'
+	
+	mailsendCommand = [mailsend, '-f', configDict['SOURCE_MAIL'], '-t', configDict['DESTINATION_MAIL'], '-smtp', configDict['SMTP_SERVER'], '-port', configDict['SMTP_PORT'], '-sub', subject, '-M', message]
+	
+	try:
+		if len(configDict['SMTP_USER']) > 0 and len(confictDict['SMTP_PASSWORD']) > 0:
+			mailsendCommand.append(['-auth', '-user', configDict['SMTP_USER'], '-pass', configDict['SMTP_PASSWORD']])
+	except:
+		logger.debug("Can't add mail authentication")
+
+	try:
+		if len(configDict['SMTP_USER']) > 0 and len(confictDict['SMTP_PASSWORD']) > 0:
+			if configDict['SECURITY'] == "tls":
+				mailsendCommand.append(['-starttls'])
+			elif configDict['SECURITY'] == "ssl":
+				mailsendCommand.append(['-ssl'])
+	except:
+		logger.debug("Can't figure out mail security settings")
+
+	print(mailsendCommand)
+
+	pHandle = Popen(mailsendCommand, stdout=PIPE, stderr=PIPE)
+	output, err = pHandle.communicate()
+	if not pHandle.returncode == 0:
+		msg="Sending mail failed:\r\n" + output.decode() + "\r\n" + err.decode()
+		logger.error(msg)
+		messagebox.showinfo('Error', msg)
+		return False
+		
+	messagebox.showinfo('Sending mail succeed. Please check your inbox.')	
+				
+def usage():
+	print(_CONSTANT.APP_NAME + " v" + _CONSTANT.APP_VERSION + " " + _CONSTANT.APP_BUILD)
+	print(_CONSTANT.AUTHOR)
+	print(_CONSTANT.CONTACT)
+	print("")
+	print("Works on Windows only")
+	print("Usage:\n")
+	print(_CONSTANT.APP_NAME + " -c [c:\\path\\to\\" + _CONSTANT.ERRORACTION_CMD_FILENAME + "] [OPTIONS]")
+	print("")
+	print("[OPTIONS]")
+	print("-f [..]              Specify a source email for alerts")
+	print("-t [..]              Destination email for alerts")
+	print("-s [..]              SMTP server address")
+	print("-P [..]              SMTP server port")
+	print("-u [..]              SMTP server username")
+	print("-p [..]              SMTP server password")
+	print("--security=[..]      SMTP server security. Valid values are: none, ssl, tls")
+	print("-z                   Compress log files before sending")
+	print("-l                   Show smart warnings on screen")
+	print("--warning=\"[..]\"     Specify a warning message that will be used for alerts")
+	print("--help, -h, -?       Will show this message")
+	print("")
+	print("Example:")
+	print(_CONSTANT.APP_NAME + " -c \"C:\Program Files\smartmontools for windows\bin\erroraction_config.cmd\" -f infra@example.tld -t monitoring@example.tld -s my.smtp.server.tld -P 587 -u infra@example.tld -p MyPassword -z")
+	sys.exit(1)
+
+def main(argv):
+	global CONFIG
+	
+	if _CONSTANT.IS_STABLE == False:
+		logger.debug("Warning: This is an unstable developpment version.")
+	
+	configDict={}
+	
+	try:
+		opts, args = getopt.getopt(argv, "hlz?c:?f:?t:?s:u:?p:?P:?", [ 'security=', 'warning=' ])
+	except getopt.GetoptError:
+		usage()
+	for opt, arg in opts:
+		if opt == '-h' or opt == "--help" or opt == "-?":
+			usage()
+		elif opt == '-c':
+			confFile = arg
+		elif opt == '-f':
+			configDict['SOURCE_MAIL'] = arg
+		elif opt == '-t':
+			configDict['DESTINATION_MAIL'] = arg
+			configDict['MAIL_ALERT'] = "yes"
+		elif opt == '-s':
+			configDict['SMTP_SERVER'] = arg
+		elif opt == '-P':
+			configDict['SMTP_PORT'] = arg
+		elif opt == '-u':
+			configDict['SMTP_USER'] = arg
+		elif opt == '-p':
+			configDict['SMTP_PASSWORD'] = arg
+		elif opt == '--security' and (arg == "none" or arg == "tls" or arg == "ssl"):
+			configDict['SECURITY'] = arg
+		elif opt == '-l':
+			configDict['LOCAL_ALERT'] = "yes"
+		elif opt == '--warning':
+			configDict['WARNING_MESSAGE'] = arg
+		elif opt == '-z':
+			configDict['COMPRESS_LOGS'] = "yes"
+	
+	# Mailer variable for compat
+	configDict['MAILER'] = 'mailsend'
+
+
+	if 'confFile' in locals():
+		CONFIG = Configuration(confFile)
+	else:
+		CONFIG = Configuration('')
+
+	try:
+		root = tk.Tk()
+		root.title(_CONSTANT.APP_NAME)
+		app = Application(root, configDict)
+		root.mainloop()
+	except Exception as e:
+		logger.critical("Cannot instanciate main tk app.")
+		logger.debug(e)
+		sys.exit(1)
+
+
+if __name__ == '__main__':
+	main(sys.argv[1:])
+	
---
+++ setup-erroraction_config.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# setup.py
+import sys
+from distutils.core import setup
+import py2exe
+
+sys.argv.append("py2exe")
+
+setup(
+    #options = {'py2exe': {'optimize': 2}},
+    windows = [{'script': "erroraction_config.py",
+				'icon_resources': [(1, "myicon.ico")]}],
+    
+    zipfile = "shared.lib",
+    #console=["smartd-pyngui.py"],
+    data_files=[("", ["erroraction_config.ui"])],
+    options= {
+        "py2exe": { 
+            "includes" : ["sys",
+						  "pygubu.builder.tkstdwidgets",
+                          "pygubu.builder.ttkstdwidgets",
+                          "pygubu.builder.widgets.dialog",
+                          "pygubu.builder.widgets.editabletreeview",
+                          "pygubu.builder.widgets.scrollbarhelper",
+                          "pygubu.builder.widgets.scrolledframe",
+                          "pygubu.builder.widgets.tkscrollbarhelper",
+                          "pygubu.builder.widgets.tkscrolledframe",
+                          "pygubu.builder.widgets.pathchooserinput",],
+            "excludes" : ["_ssl",
+                          "pyreadline",
+                          "difflib",
+                          "doctest",
+                          "locale",
+                          "optparse",
+                          "calendar",],
+            "compressed" : True,
+                          
+                    }},
+        )
